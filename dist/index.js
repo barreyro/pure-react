f0eff90d95ce4853ba5760ecc0f3389f7bb18872
Stephanie Barreyro <stephaniebarreyro@gmail.com>
2017-05-07 23:00:43 -0400

    Render pure react

---
+++ dist/index.js
@@ -0,0 +1,14 @@
+const { createElement } = React
+const { render } = ReactDOM
+
+const title = React.createElement(
+  'h1',
+  {id: 'title', className: 'header'},
+  'Hello World'
+);
+
+
+ReactDOM.render(
+  title,
+  document.getElementById('react-container')
+)
