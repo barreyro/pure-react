c6166cda585895dc840efa7472ed37c77b8ea261
Stephanie Barreyro <stephaniebarreyro@gmail.com>
2017-05-09 21:55:16 -0400

    Add component props

--- MakeUpDayCounts.js
+++ MakeUpDayCounts.js
@@ -6,13 +6,19 @@ export const MakeUpDayCount = React.createClass({
     return (
       <div className="make-up-day-count">
         <div className="total-days">
-          <span> 5 days</span>
+          <span> {this.props.total}</span>
+          <span>  days</span>
         </div>
         <div className="concealer-days">
-          <span> 2 days</span>
+          <span> {this.props.concealer}</span>
+          <span>  days</span>
         </div>
         <div className="foundation-days">
-          <span> 2 days</span>
+          <span> {this.props.foundation}</span>
+          <span>  days</span>
+        </div>
+        <div>
+          <span>{this.props.goal}</span>
         </div>
       </div>
     )
--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ import { MakeUpDayCount } from '../MakeUpDayCounts';
 window.Reaact = React
 
 render(
-  <MakeUpDayCount/>,
+  <MakeUpDayCount total={50}
+                  concealer={20}
+                  foundation={10}
+                  goal={100}/>,
   document.getElementById('react-container')
 )
