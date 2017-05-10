2101da43dc28c92a1b0e84735307e8673dfd26bb
Stephanie Barreyro <stephaniebarreyro@gmail.com>
2017-05-09 22:30:47 -0400

    Add react-icons

--- MakeUpDayCounts.js
+++ MakeUpDayCounts.js
@@ -1,4 +1,7 @@
 import './src/stylesheets/ui.scss';
+import Terrain from 'react-icons/lib/md/terrain';
+import SnowFlake from 'react-icons/lib/ti/weather-snow';
+import Calendar from 'react-icons/lib/fa/calendar';
 
 const percentToDecimal = (decimal) => {
   return ((decimal * 100) + '%')
@@ -12,14 +15,18 @@ export const MakeUpDayCount = ({total, concealer, foundation, goal}) => (
   <div className="make-up-day-count">
     <div className="total-days">
       <span>{total}</span>
+        <Calendar />
       <span>days</span>
+        
     </div>
     <div className="concealer-days">
       <span>{concealer}</span>
+        <SnowFlake />
       <span>days</span>
     </div>
     <div className="foundation-days">
       <span>{foundation}</span>
+        <Terrain />
       <span>days</span>
     </div>
     <div>
